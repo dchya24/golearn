6f6a725d3cecf5f306b01c4f1bd6306956149f52
dchya24 <cahyadinar241@gmail.com>
2023-10-26 16:03:28 +0700

    Create simple CRUD Todo with array

--- go.mod
+++ go.mod
@@ -10,7 +10,8 @@ require (
 require (
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.31.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
--- main.go
+++ main.go
@@ -1,29 +1,82 @@
 package main
 
 import (
-	"net/http"
-
 	"fmt"
+	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
+type Todo struct {
+	Title       string
+	Description string
+}
+
 type Response struct {
-	Message string `json:"message" xml:"message"`
+	Status  string
+	Message string
+}
+
+var todos = []Todo{
+	{"Learn Go", "Learn Go from Internet"},
+	{"Learn Blockchain", "Learn Blockchain for crypto"},
 }
 
 func main() {
-	fmt.Println("Hello")
-
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
-		response := Response{
-			Message: "Hello World",
-		}
+		id := c.QueryParams().Get("id")
 
-		return c.JSON(http.StatusOK, response)
+		fmt.Printf("Id is: %s \n", id)
+
+		index, _ := strconv.Atoi(id)
+
+		if id != "" {
+			var todo Todo = todos[index-1]
+
+			return c.JSON(http.StatusOK, todo)
+		}
+		return c.JSON(http.StatusOK, todos)
 	})
 
-	e.Logger.Fatal(e.Start(":8000"))
+	e.GET("/add", func(c echo.Context) error {
+		title := c.QueryParams().Get("title")
+		description := c.QueryParams().Get("description")
+
+		var newTodo Todo = Todo{title, description}
+
+		todos = append(todos, newTodo)
+
+		response := Response{"Ok", "Success Add Todo"}
+
+		return c.JSON(http.StatusAccepted, response)
+	})
+
+	e.GET("update/:id", func(c echo.Context) error {
+		id, _ := strconv.Atoi(c.Param("id"))
+		title := c.QueryParams().Get("title")
+		description := c.QueryParams().Get("description")
+
+		var updateTodo Todo = Todo{title, description}
+
+		todos[id-1] = updateTodo
+
+		response := Response{"Ok", "Todo updated success!"}
+
+		return c.JSON(http.StatusAccepted, response)
+	})
+
+	e.GET("delete", func(c echo.Context) error {
+		idParam := c.QueryParams().Get("id")
+
+		id, _ := strconv.Atoi(idParam)
+
+		todos = append(todos[:id], todos[id+1:]...)
+
+		return c.JSON(http.StatusAccepted, todos)
+	})
+
+	e.Logger.Fatal(e.Start(":8001"))
 }

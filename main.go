3498c61235b96e97415b70f8fea9716b215a6094
dchya24 <cahyadinar241@gmail.com>
2023-10-31 17:00:02 +0700

    refactor code

--- main.go
+++ main.go
@@ -23,60 +23,68 @@ var todos = []Todo{
 	{"Learn Blockchain", "Learn Blockchain for crypto"},
 }
 
+func GetTodo(c echo.Context) error {
+	id := c.QueryParams().Get("id")
+
+	fmt.Printf("Id is: %s \n", id)
+
+	index, _ := strconv.Atoi(id)
+
+	if id != "" {
+		var todo Todo = todos[index-1]
+
+		return c.JSON(http.StatusOK, todo)
+	}
+	return c.JSON(http.StatusOK, todos)
+}
+
+func AddTodo(c echo.Context) error {
+	title := c.QueryParams().Get("title")
+	description := c.QueryParams().Get("description")
+
+	var newTodo Todo = Todo{title, description}
+
+	todos = append(todos, newTodo)
+
+	response := Response{"Ok", "Success Add Todo"}
+
+	return c.JSON(http.StatusAccepted, response)
+}
+
+func UpdateTodo(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	title := c.QueryParams().Get("title")
+	description := c.QueryParams().Get("description")
+
+	var updateTodo Todo = Todo{title, description}
+
+	todos[id-1] = updateTodo
+
+	response := Response{"Ok", "Todo updated success!"}
+
+	return c.JSON(http.StatusAccepted, response)
+}
+
+func DeleteTodo(c echo.Context) error {
+	idParam := c.QueryParams().Get("id")
+
+	id, _ := strconv.Atoi(idParam)
+
+	todos = append(todos[:id], todos[id+1:]...)
+
+	return c.JSON(http.StatusAccepted, todos)
+}
+
 func main() {
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		id := c.QueryParams().Get("id")
+	e.GET("/", GetTodo)
 
-		fmt.Printf("Id is: %s \n", id)
+	e.GET("/add", AddTodo)
 
-		index, _ := strconv.Atoi(id)
+	e.GET("update/:id", UpdateTodo)
 
-		if id != "" {
-			var todo Todo = todos[index-1]
-
-			return c.JSON(http.StatusOK, todo)
-		}
-		return c.JSON(http.StatusOK, todos)
-	})
-
-	e.GET("/add", func(c echo.Context) error {
-		title := c.QueryParams().Get("title")
-		description := c.QueryParams().Get("description")
-
-		var newTodo Todo = Todo{title, description}
-
-		todos = append(todos, newTodo)
-
-		response := Response{"Ok", "Success Add Todo"}
-
-		return c.JSON(http.StatusAccepted, response)
-	})
-
-	e.GET("update/:id", func(c echo.Context) error {
-		id, _ := strconv.Atoi(c.Param("id"))
-		title := c.QueryParams().Get("title")
-		description := c.QueryParams().Get("description")
-
-		var updateTodo Todo = Todo{title, description}
-
-		todos[id-1] = updateTodo
-
-		response := Response{"Ok", "Todo updated success!"}
-
-		return c.JSON(http.StatusAccepted, response)
-	})
-
-	e.GET("delete", func(c echo.Context) error {
-		idParam := c.QueryParams().Get("id")
-
-		id, _ := strconv.Atoi(idParam)
-
-		todos = append(todos[:id], todos[id+1:]...)
-
-		return c.JSON(http.StatusAccepted, todos)
-	})
+	e.GET("delete", DeleteTodo)
 
 	e.Logger.Fatal(e.Start(":8001"))
 }
